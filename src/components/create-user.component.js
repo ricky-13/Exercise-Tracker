2a58bb7bb752564483aedd5f156f8e576413bc35
Ricky <ahujashaurya13@gmail.com>
2023-06-01 13:07:28 -0400

    created the required components

---
+++ src/components/create-user.component.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+export default class ExercisesList extends Component {
+    render() {
+        <div>
+            <p>You are on the Exercises List component!</p>
+        </div>
+    }
+}

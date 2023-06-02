f393b4357b8e518d1acfda549505ab27a02b4cf9
Ricky <ahujashaurya13@gmail.com>
2023-06-02 12:04:29 -0400

    bug fixes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 import Navbar from "./components/navbar.component.js";
-import ExerciseList from "./components/exercise-list.component";
+import ExerciseList from "./components/exercise-list.component.js";
 import EditExercise from "./components/edit-exercise.component";
 import CreateExercise from "./components/create-exercise.component";
 import CreateUser from "./components/create-user.component";
@@ -14,10 +14,12 @@ function App() {
     <Router>
       <Navbar />
       <br/>
-      <Route path="/" exact component = {ExerciseList} />
-      <Route path="/edit/:id" exact component = {EditExercise} />
-      <Route path="/create" exact component = {CreateExercise} />
-      <Route path="/user" exact component = {CreateUser} />
+      <Routes>
+        <Route path="/" element = {<ExerciseList />} />
+        <Route path="/edit/:id" element = {<EditExercise />} />
+        <Route path="/create" element = {<CreateExercise />} />
+        <Route path="/user" element = {<CreateUser />} />
+      </Routes>
     </Router>
   );
 }

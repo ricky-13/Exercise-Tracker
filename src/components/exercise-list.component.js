6ceecb5fc15acc97a1a8c8f1204508a02939b94f
Ricky <ahujashaurya13@gmail.com>
2023-06-21 19:55:04 -0400

    adding changes to exercise list component

--- src/components/exercise-list.component.js
+++ src/components/exercise-list.component.js
@@ -2,6 +2,19 @@ import React, { Component } from 'react';
 import {Link} from 'react-router-dom';
 import axios from 'axios';
 
+const Exercise = props => (
+    <tr>
+        <td>{props.exercise.username}</td>
+        <td>{props.exercise.description}</td>
+        <td>{props.exercise.duration}</td>
+        <td>{props.exercise.date.substring(0,10)}</td>
+        <td>
+            <Link to = {"/edit/" + props.exercise._id}>edit</Link> | <a href = "#" onClick = {() => { props.deleteExercise()}}></a>
+            {/* yet to change */}
+        </td>
+    </tr>
+)
+
 export default class ExerciseList extends Component {
     constructor(props){
         super(props);
@@ -27,6 +40,13 @@ export default class ExerciseList extends Component {
 
         this.setState({
             exercises: this.state.exercises.filter(el => el._id !== id)
+        })
+    }
+
+    exerciseList(){
+        return this.state.exercises.map(currentexercise => {
+            return <Exercise exercise = { currentexercise } deleteExercise = { this.deleteExercise } key = { currentexercise._id } />
+            // might have to add semi colon at the end 
         })
     }
     render() {
